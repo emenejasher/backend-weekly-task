0e6d56aedc8de27cdb2dc4aee1c67d478e9df5bd
emenejasher <85228406+emenejasher@users.noreply.github.com>
2021-07-13 11:32:27 +0100

    Add files via upload

---
+++ week 6 Task/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const package = require('./package/package.js');
+const app = express();
+
+const packagelist = require('./package/package.js');
+
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('Successful response.');
+});
+
+app.get('/packagelist',(req, res)=>{
+    res.json(packagelist).status(201);
+   
+});
+
+app.post('/packagelist/add',(req, res) =>{
+    if(!req.body.name || !req.body.description || !req.body.image || !req.body.price){
+        res.status(400).send('u try')
+    }else{
+        let newpackagelist ={
+            id: packagelist.length + 1,
+            name: req.body.name,
+            description: req.body.description,
+            image: req.body.image,
+            price: req.body.price,
+        }
+        packagelist.push(newpackagelist);
+        res.json(packagelist);
+    }
+
+});
+
+app.put('/packagelist/:id', (req, res) =>{
+    let packagelistId = Number(req.params.id);
+    let body = req.body;
+    let packagelist = packagelist.find((packagelist) => packagelist.id === packagelistId);
+    let indexOfPackagelist = packagelist.indexOf(packagelist);
+    if(!packagelist){
+        res.status(404).send(`Packagelist with id of ${packagelistId} not found`)
+    }else{
+        let updatePackagelist = {...packagelist, ...body};
+        packagelist[indexOfPackagelist] = updatePackagelist;
+        res.json(updatePackagelist)
+    }
+})
+
+app.delete('/packagelist/:id',(req,res) =>{
+    let packagelistId = Number(req.params.id);
+    let deletePackagelist = packagelist.filter((packagelist) => packagelist.id !== packagelistId);
+    if(!deletePackagelist){
+        res.status(404).send(`Packagelist with id of ${packagelistId} not found`);
+    }else{
+        packagelist = deletePackagelist;
+        res.json(packagelist);
+    }
+})
+
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
---
+++ week 6 Task/package/package.js
@@ -0,0 +1,73 @@
+const package = [
+    {
+        id: 1,
+        name: "HP 15 Laptop",
+        description: "8GB RAM, 1TB HD, 15.6 HD TOUCH, FREEDOS",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL0O6GUVNAFAMZ/image/1/",
+        price: "#206,970"
+    },
+    {
+        id: 2,
+        name: "HP 15 AMD",
+        description: "8GB RAM, 256GB HD, WINDOWS 10",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL0T6N5TNAFAMZ/image/1/",
+        price: "#152,000"
+    },
+    {
+        id: 3,
+        name: "LENOVO IDEAPAD",
+        description: "8GB RAM, 1TB HD,WINDOWS 10",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL0O6GUVNAFAMZ/image/1/",
+        price: "#172,500"
+    },
+    {
+        id: 4,
+        name: "HP STREAM 11",
+        description: "4GB RAM, 32GB HD, INTEL CELERON",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/LE842CL03YTAVNAFAMZ/image/1/",
+        price: "#115,000"
+    },
+    {
+        id: 5,
+        name: "HP PROBOOK",
+        description: "4GB RAM, 128GB HD, WINDOWS 10",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL0W3GQZNAFAMZ/image/1/",
+        price: "#155,000"
+    },
+    {
+        id: 6,
+        name: "HP STREAM 12",
+        description: "2GB RAM, 32GB HD, FREEDOS",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL0UH1OFNAFAMZ/image/1/",
+        price: "#119,000"
+    },
+    {
+        id: 7,
+        name: "HP STREAM 13",
+        description: "4GB RAM, 32GB HD, MAC",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/LE842CL09A0XPNAFAMZ/image/1/",
+        price: "#132,000"
+    },
+    {
+        id: 8,
+        name: "HP ENVY 12",
+        description: "8GB RAM, 1TB HD, 15.6 HD TOUCH, WIN 10",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL190EPWNAFAMZ/image/1/",
+        price: "#306,970"
+    },
+    {
+        id: 9,
+        name: "HP 17 Laptop",
+        description: "8GB RAM, 1TB HD, FREEDOS",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL00LQJLNAFAMZ/image/3/",
+        price: "#206,970"
+    },
+    {
+        id: 10,
+        name: "LENOVO IDEAPAD Z",
+        description: "8GB RAM, 500GB HD, 15.6 HD TOUCH, FREEDOS",
+        image:"https://www.jumia.com.ng/catalog/productimagezoom/sku/HP246CL1BZT1ZNAFAMZ/image/2/",
+        price: "#156,970"
+    }];
+    
+    module.exports = package ;
